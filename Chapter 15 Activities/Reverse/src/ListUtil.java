1ae0411822ae054b501ce2e16de7d2e77103c72c
Arthur <azxu@stu.naperville203.org>
2024-08-30 13:33:59 -0500

    Finished Reverse

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -16,7 +16,7 @@ public class ListUtil
         ListIterator<String> iterator = strings.listIterator();
         int l = strings.size();
 
-        for (int i = 0; i < l/2; i++)
-            strings.set(i, strings.set(l - i - 1, iterator.next()));
+        for (int i = 0; i < l/2; i++)//The idea is to swap the first and last elements, the second and second-to-last elements, etc.
+            strings.set(i, strings.set(l - i - 1, iterator.next()));//Here, I iterate through the first half of the list and swap the ith element with the l-i-1th element, where it is when reversed.       
     }
 }
